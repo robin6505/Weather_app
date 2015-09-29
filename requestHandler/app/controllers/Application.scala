16e2cfde3d79f33fd9bbcd751a301090176a7d10
robin6505 <robin.koezen@gmail.com>
2015-09-29 10:51:51 +0200

    final fix api calls

--- requestHandler/app/controllers/Application.scala
+++ requestHandler/app/controllers/Application.scala
@@ -23,47 +23,29 @@ class Application @Inject() (ws: WSClient) extends Controller {
         
     val apiKey = "&APPID=2058452e3cf07873e426f1d723339ec6"
     
-    
-    
-
-    
     //getWeather function
     def getWeather(city: String) : Future[JsValue] = {
         val url = (s"http://api.openweathermap.org/data/2.5/weather?q=$city$apiKey")
         val request = WS.url(url).get
-
         request map{response => response.json}
     }
-    
     
     def getForecast(city: String) : Future[JsValue] = {
         val url = (s"http://api.openweathermap.org/data/2.5/forecast/daily?q=$city&cnt=6&units=metric$apiKey")
         val request = WS.url(url).get
-
         request map{response => response.json}
     }
     
-    def getData(city: String) = Action {
-        val resp = getWeather(city: String)
-        println(resp)
-        
-        val resp2 = getForecast(city: String)
-        println(resp2)
-        
-        //should be possible without await, could give timeout on first try
-        val current = Await.result(resp, 500 milliseconds)
-        val forecast = Await.result(resp2, 500 milliseconds)
-        val weather = current.as[JsObject] ++ forecast.as[JsObject]
-        val weather2 = Json.obj(
-            "CURRENTWEATHER" -> current,
-            "FORECAST" -> forecast)
-        
-        Ok(weather2)
+    def getData(city: String) = Action.async {
+        for {
+            resp <- getWeather(city: String)
+            resp2 <- getForecast(city: String)
+            
+        } yield Ok(Json.obj(
+            "CURRENTWEATHER" -> resp,
+           "FORECAST" -> resp2))
     }
   
-
-
-
     def index = Action {
         Ok(views.html.index("Your new application is ready."))
     }
