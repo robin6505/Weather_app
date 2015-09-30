b940494ec9bcb150485da8c367a0fdb5ba67936a
robin6505 <robin.koezen@gmail.com>
2015-09-30 11:39:45 +0200

    mongo connection

--- requestHandler/app/controllers/Application.scala
+++ requestHandler/app/controllers/Application.scala
@@ -8,20 +8,67 @@ import play.api.Play.current
 import play.api.mvc._
 import play.api.libs.ws._
 
-import play.api._
-import play.api.mvc._
- 
 
 import play.api.libs.concurrent.Execution.Implicits._
 import java.util.concurrent.TimeoutException
 
-import scala.concurrent.duration._
-import scala.concurrent.Await
+import reactivemongo.api._
+import scala.concurrent.ExecutionContext.Implicits.global
 
+import play.api.libs.iteratee.Iteratee
+import reactivemongo.bson.BSONDocument
+import reactivemongo.api.collections.bson.BSONCollection
 
 class Application @Inject() (ws: WSClient) extends Controller {
+    
         
     val apiKey = "&APPID=2058452e3cf07873e426f1d723339ec6"
+    
+    val driver = new MongoDriver
+    val connection = driver.connection(List("localhost"))
+    val db = connection("test")
+    val collection = db("places")
+    
+    def listDocs(collection: BSONCollection) = {
+      // Select only the documents which field 'firstName' equals 'Jack'
+      val query = BSONDocument("places" -> "Zwolle")
+
+    
+      /* Let's run this query then enumerate the response and print a readable
+       * representation of each document in the response */
+      collection.
+        find(query).
+        cursor[BSONDocument].
+        enumerate().apply(Iteratee.foreach { doc =>
+          println(s"found document: ${BSONDocument pretty doc}")
+        })
+    
+      // Or, the same with getting a list
+      //val futureList: Future[List[BSONDocument]] =
+        //collection.
+          //find(query, filter).
+          //cursor[BSONDocument].
+        //  collect[List]()
+    
+      //futureList.map { list =>
+        //list.foreach { doc =>
+          //println(s"found document: ${BSONDocument pretty doc}")
+        //}
+      //}
+    }
+    
+    
+    
+    //def findPlaces = Action {
+        // let's do our query
+      //  val cursor = collection.
+          // find all
+        //  find(Json.obj())
+          // sort them by creation date
+          //println(cursor)
+          // perform the query and get a cursor of JsObject
+         // Ok("lala")
+    //  }
     
     //getWeather function
     def getWeather(city: String) : Future[JsValue] = {
@@ -37,13 +84,14 @@ class Application @Inject() (ws: WSClient) extends Controller {
     }
     
     def getData(city: String) = Action.async {
+        listDocs(collection: BSONCollection)
         for {
             resp <- getWeather(city: String)
             resp2 <- getForecast(city: String)
             
         } yield Ok(Json.obj(
             "CURRENTWEATHER" -> resp,
-           "FORECAST" -> resp2))
+            "FORECAST" -> resp2))
     }
   
     def index = Action {
--- requestHandler/build.sbt
+++ requestHandler/build.sbt
@@ -11,7 +11,7 @@ libraryDependencies ++= Seq(
   cache,
   ws,
   specs2 % Test,
-  "org.reactivemongo" %% "play2-reactivemongo" % "0.10.5.0.akka23"
+  "org.reactivemongo" %% "reactivemongo" % "0.11.7"
 )
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
@@ -20,6 +20,10 @@ resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
 // other, legacy style, accesses its actions statically.
 routesGenerator := InjectedRoutesGenerator
 
+
+fork in run := true
+
+fork in run := true
 
 fork in run := true
 
