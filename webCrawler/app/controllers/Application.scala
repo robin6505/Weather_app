773ef65306cf843d0c2e8f43a49c41e3c42de578
Robin <robin.koezen@gmail.com>
2015-10-20 14:32:35 +0200

    recent changes webcrawler with etcd

--- webCrawler/app/controllers/Application.scala
+++ webCrawler/app/controllers/Application.scala
@@ -36,6 +36,7 @@ import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
 import ExecutionContext.Implicits.global
 import java.util.Calendar
+import reactivemongo.core.nodeset.Authenticate
 
 
 class Application @Inject() (val reactiveMongoApi: ReactiveMongoApi) 
@@ -50,52 +51,85 @@ class Application @Inject() (val reactiveMongoApi: ReactiveMongoApi)
     
     //Nog toevoegen timer interval en tijd etc aan object toevoegen
     def crawlData() = {
+		println("Executes function in interval test")
+		
+		//First get available mongo instances
+		val url = (s"http://127.0.0.1:4001/v2/keys/announce/services/mongo")
+        val request = WS.url(url).get
+        request map{response => 
+            println(response.json) //when mongo is retrieved do below
+            val mongoresp = response.json
+            val serversResp = (mongoresp \\ "value").map(_.as[String])
+            println("These are the ips: " + serversResp)
+        
+			//create time stamp for each set of retrieved data
+			val timestamp = Calendar.getInstance().getTime()
+			
+        
+			def driver: reactivemongo.api.MongoDriver = ???
+			val servers = serversResp
+			
+			//connect to server with authorization
+			val dbName = "test"
+			val userName = "siteRootAdmin"
+			val password = "password"
+			val credentials = List(Authenticate(dbName, userName, password))
+			val connection = driver.connection(servers, authentications = credentials)
+        
+        
+        
+			//val driver = new MongoDriver
+			//def driver: reactivemongo.api.MongoDriver = ???
+			
+			//val connection = driver.connection(servers)
+			val db = connection("test")
+			
+			//First retrieve all the city names stored in the database
+			val collectionGet = db("places")
+			
+			//op deze manier kun je er meteen JSON in gooien. Met alles doen?
+			val collectionStore: JSONCollection = db("measurements")
+			
+			val query = BSONDocument()
+			// select only the fields 'name'
+			val filter = BSONDocument("name" -> 1, "_id" -> 0)
+			//implicit def authTimeout: scala.concurrent.duration.FiniteDuration = ???
+			//val futureAuthenticated = db.authenticate("siteRootAdmin", "password")
 
-		val timestamp = Calendar.getInstance().getTime()
-		println(timestamp)
-        println("Executes function in interval test")
-        val driver = new MongoDriver
-        val connection = driver.connection(List("95.85.2.45"))
-        val db = connection("test")
-        
-        //First retrieve all the city names stored in the database
-        val collectionGet = db("places")
-        
-        //op deze manier kun je er meteen JSON in gooien. Met alles doen?
-        val collectionStore: JSONCollection = db("measurements")
-        
-        val query = BSONDocument()
-        // select only the fields 'name'
-        val filter = BSONDocument("name" -> 1, "_id" -> 0)
-        
-        val futureList: Future[List[BSONDocument]] =
-            collectionGet.
-              find(query, filter).
-              cursor[BSONDocument].
-              collect[List]()
-        
-        //Kijken of dit mooier kan? werkt nu
-          futureList.map { list =>
-			val uniqueList = list.distinct
-            uniqueList.foreach { doc =>
-              println(s"found document: ${BSONDocument pretty doc}")
-              println(doc.get("name"))
-              doc.get("name") match {
-                  case Some(BSONString(name)) => {
-                      val current = getWeather(name)
-                      current.map {
-                          resp => {
-                              //println(resp)
-							  val combined = Json.obj("TIMESTAMP" -> timestamp, "CONDITION" -> resp)
-							  //println(combined)
-							  collectionStore.insert(combined.as[JsObject])
-                          }
-                      }
-                  }
-                  case _ => println("Could not get city name")
-              }
-            }
-          }
+			  //futureAuthenticated.map { _ =>
+				// doSomething
+			  
+			
+			val futureList: Future[List[BSONDocument]] =
+				collectionGet.
+				  find(query, filter).
+				  cursor[BSONDocument].
+				  collect[List]()
+			
+			//Kijken of dit mooier kan? werkt nu
+			  futureList.map { list =>
+				val uniqueList = list.distinct
+				uniqueList.foreach { doc =>
+				  println(s"found document: ${BSONDocument pretty doc}")
+				  println(doc.get("name"))
+				  doc.get("name") match {
+					  case Some(BSONString(name)) => {
+						  val current = getWeather(name)
+						  current.map {
+							  resp => {
+								  //println(resp)
+								  val combined = Json.obj("TIMESTAMP" -> timestamp, "CONDITION" -> resp)
+								  //println(combined)
+								  collectionStore.insert(combined.as[JsObject])
+							  }
+						  }
+					  }
+					  case _ => println("Could not get city name")
+				  }
+				}
+			  }
+		  //} closes db.auth future method
+	  } //closes etcd mongo ip request
     }
     
     def start() = {
