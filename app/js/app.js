50f27f0781b74228beb164babb4b53c98e7e3bc8
Frank Blaauw <frank.blaauw@gmail.com>
2015-10-31 13:21:38 +0100

    Do not merge this! it is just an example

--- app/js/app.js
+++ app/js/app.js
@@ -1,31 +1,31 @@
 var weatherApp = angular.module('weatherApp', ['ui.router']);
 
 weatherApp.config(function($stateProvider, $urlRouterProvider) {
-    
-    $stateProvider
-        
-        // HOME STATES AND NESTED VIEWS ========================================
-        .state('main', {
-            url: '/main',
-            templateUrl: 'templates/main.html'
-            
-        })
-        .state('result', {
-            url: '/result',
-            //controller: 'SimpleCtrl',
-            views: {
 
-                // the main template will be placed here (relatively named)
-                '': { templateUrl: 'templates/results.html' },
+  $stateProvider
 
-                // the child views will be defined here (absolutely named)
-                'currentBox@result': { templateUrl: 'templates/currentWeather.html', controller: 'SimpleCtrl' },
-                'forecastBox@result': { templateUrl: 'templates/forecast.html', controller: 'SimpleCtrl' },
-                'pastBox@result': { templateUrl: 'templates/pastDays.html' },
-                'livestreamBox@result': { templateUrl: 'templates/livestream.html', controller: 'SimpleCtrl' },
-                'analysisBox@result': { templateUrl: 'templates/weatherAnalysis.html', controller: 'SimpleCtrl' }                
-            }
-        })        
-        $urlRouterProvider.otherwise('/main');
-        
-})
+  // HOME STATES AND NESTED VIEWS ========================================
+    .state('main', {
+      url: '/main',
+      templateUrl: 'templates/main.html'
+
+    })
+    .state('result', {
+      url: '/result',
+      //controller: 'SimpleCtrl',
+      views: {
+
+        // the main template will be placed here (relatively named)
+        '': {
+          templateUrl: 'templates/results.html'
+        },
+
+        'livestreamBox@result': {
+          templateUrl: 'templates/livestream.html',
+          controller: 'SimpleCtrl'
+        }
+      }
+    });
+  $urlRouterProvider.otherwise('/main');
+
+});
--- app/js/simpleController.js
+++ app/js/simpleController.js
@@ -1,82 +1,20 @@
+var ws;
 angular.module('weatherApp')
 
 .controller('SimpleCtrl', function($scope, $state, SessionService, $timeout, $interval) {
-    $scope.today = new Date();
-    $scope.weatherCurrent;
-    $scope.imageLink = 'http://openweathermap.org/img/w/01d.png';
-    $scope.forecast;
-    $scope.imageLinkF = [];
-    $scope.nextDays = [];
-    $scope.liveStream;
-    $scope.imageLinkLS;
-    $scope.analytics = [[], [], [],[]];
-        
-    //Create days of coming week
-    for (j = 1; j < 7; j++) { 
-        var nextD = new Date();    
-        nextD.setDate(nextD.getDate()+j);
-        $scope.nextDays.push(nextD);
-    }
-    
-    
-    //Gets Weather Data
-    SessionService.getWeather().then(function() {
-		//Get current Weather
-        $scope.weatherCurrent = SessionService.weatherCurrent();
-        console.log($scope.weatherCurrent);
-        //create imagelink for current weather
-        $scope.imageLink = 'http://openweathermap.org/img/w/' + $scope.weatherCurrent['weather'][0].icon + ".png";
-        
-        //Get Forecast
-        $scope.forecast = SessionService.weatherForecast();
-        //Create imagelink for forecasts
-        for (i = 0; i < $scope.forecast.list.length; i++) {
-            $scope.imageLinkF.push('http://openweathermap.org/img/w/' + $scope.forecast['list'][i]['weather'][0].icon + ".png");
-        }
-        
-        //Get analytics
-        var analyticData = SessionService.analytics();
-        //Divide in four temperature groups
-        console.log("DIT IS ANALYTIC DATA:" +analyticData)
-        for (i = 0; i<analyticData.length; i++) {
-            if (analyticData[i].value.tempGroup < 0) {
-                $scope.analytics[0].push(analyticData[i]._id)
-            } else if (analyticData[i].value.tempGroup < 3) {
-                $scope.analytics[analyticData[i].value.tempGroup].push(analyticData[i]._id)
-            } else {
-                $scope.analytics[3].push(analyticData[i]._id)
-            }
-        }
-        console.log($scope.analytics)
-        
-    }, function(err) {
-        console.log('Geen info opgehaald');
-    });
-    
-    //SessionService.getForecast().then(function() {
-      //  $scope.forecast = SessionService.weatherForecast();
-        //create imagelink for weatherForecast
-        //for (i = 0; i < $scope.forecast.list.length; i++) {
-         //   $scope.imageLinkF.push('http://openweathermap.org/img/w/' + $scope.forecast['list'][i]['weather'][0].icon + ".png");
-        //}
-   // }, function(err) {
-     //   console.log('Geen info opgehaald');
-    //});
-    
-    var getLiveStream = function() {
-        ws = new WebSocket('ws://localhost:9000/stream')
-        ws.onmessage = function( message ) { 
-            console.log( message );
-            $scope.liveStream = JSON.parse(message.data);
-            console.log($scope.liveStream);
-            $scope.imageLinkLS = 'http://openweathermap.org/img/w/' + $scope.liveStream['weather'][0].icon + ".png";
-            }
-            console.log($scope.liveStream);
-            //livestream does not come out of the onmessage stream
-        
-            //create imagelink for weather
-            //
-         
-    };
-    getLiveStream();
-})
+
+  $scope.updateMe = function() {
+    $scope.liveStream = "grunn";
+  };
+
+  updateData = function(message) {
+    console.log('called!');
+    $scope.liveStream = message.data;
+    $scope.liveStream.name = "gro.ningen";
+    $scope.$apply();
+  };
+
+  console.log('Livestream opened!');
+  ws = new WebSocket('wss://echo.websocket.org');
+  ws.onmessage = updateData;
+});
