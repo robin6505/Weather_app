495b90e5b124685323901d2eb0ffa1bbd1a6146d
robin6505 <robin.koezen@gmail.com>
2015-11-01 14:09:09 +0100

    added alert for blank field

--- app/js/navController.js
+++ app/js/navController.js
@@ -2,7 +2,12 @@ angular.module('weatherApp')
 
 .controller('NavCtrl', function($scope, $state, SessionService, $timeout) {
     $scope.getResults = function(city) {
-        SessionService.saveLocation(city.name);
-        $state.go('result', {}, {reload: true});
+        if(typeof city === 'undefined'){
+            console.log("leeg veld");
+            alert("Enter a city name in the text field");
+        } else {
+            SessionService.saveLocation(city.name);
+            $state.go('result', {}, {reload: true});
+        }
     }    
 })
