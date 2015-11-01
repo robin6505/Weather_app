ce3f7494740898a9469f2324698809bef2ef4948
robin6505 <robin.koezen@gmail.com>
2015-11-01 14:26:12 +0100

    cleaned up code

--- app/js/navController.js
+++ app/js/navController.js
@@ -3,7 +3,6 @@ angular.module('weatherApp')
 .controller('NavCtrl', function($scope, $state, SessionService, $timeout) {
     $scope.getResults = function(city) {
         if(typeof city === 'undefined'){
-            console.log("leeg veld");
             alert("Enter a city name in the text field");
         } else {
             SessionService.saveLocation(city.name);
